74ffeb00ec997f509dffde3c99a971671faf5428
LEEMOONKI <shinbaek89@gmail.com>
2016-08-23 23:51:01 +0900

    add users dummy test set

--- models/user.js
+++ models/user.js
@@ -42,6 +42,95 @@ function  findUser(userId, callback) {
     callback(null, user);
 }
 
+
+
+
+// models showing JSON data for dummy test
+function dummyShowMe(page, count, callback){
+  
+  var user = {
+    page: page,
+    count: count,
+    result: {
+      id: 1,
+      nickname: '닉네임',
+      image_page: '/usr/desktop/didimdol.jpg',
+      genre: '발라드',
+      post_count: 2,
+      data: [
+        {
+          id: 2,
+          user_id: 1,
+          nickname: '닉네임',
+          filetype: 0,
+          file_path: '/usr/desktop/didimdol.mp3',
+          date: '2016-08-23',
+          like: 3
+        },
+        {
+          id: 5,
+          user_id: 1,
+          nickname: '닉네임',
+          filetype: 0,
+          file_path: '/usr/desktop/didimdol2.mp3',
+          date: '2016-08-23',
+          like: 0
+        }
+      ]
+    }
+  };
+  
+  callback(null, user);
+}
+
+function dummyShowOther(id, page, count, callback){
+  
+  var user = {
+    page: page,
+    count: count,
+    result: {
+      id: id,
+      nickname: '다른 계정 닉네임',
+      image_page: '/usr/desktop/didimdol.jpg',
+      genre: '발라드',
+      post_count: 2,
+      data: [
+        {
+          id: 1,
+          user_id: id,
+          nickname: '다른 계정 닉네임',
+          filetype: 0,
+          file_path: '/usr/desktop/didimdol.mp3',
+          date: '2016-08-23',
+          like: 2
+        },
+        {
+          id: 4,
+          user_id: id,
+          nickname: '다른 계정 닉네임',
+          filetype: 0,
+          file_path: '/usr/desktop/didimdol2.mp3',
+          date: '2016-08-23',
+          like: 10
+        }
+      ]
+    }
+  };
+  
+  callback(null, user);
+}
+
+function dummyRegisterUser(user, callback){
+
+  callback(null, true);
+
+}
+
 module.exports.findByEmail = findByEmail;
 module.exports.verifyPassword = verifyPassword;
 module.exports.findUser = findUser;
+
+// models showing JSON data for dummy test
+module.exports.dummyShowMe = dummyShowMe;
+module.exports.dummyShowOther = dummyShowOther;
+module.exports.dummyRegisterUser = dummyRegisterUser;
--- routes/users.js
+++ routes/users.js
@@ -19,37 +19,71 @@ router.get('/', isAuthenticate, function(req, res, next) {
       }
     });
   }
-  res.send({
-    page: page,
-    count: count,
-    result: {
-      id: 1,
-      nickname: '닉네임',
-      image_page: '/usr/desktop/didimdol.jpg',
-      genre: '발라드',
-      post_count: 2,
-      data: [
-        {
-          id: 2,
-          user_id: 1,
-          nickname: '닉네임',
-          filetype: 0,
-          file_path: '/usr/desktop/didimdol.mp3',
-          date: '2016-08-23',
-          like: 3
-        },
-        {
-          id: 5,
-          user_id: 1,
-          nickname: '닉네임',
-          filetype: 0,
-          file_path: '/usr/desktop/didimdol2.mp3',
-          date: '2016-08-23',
-          like: 0
+  if (req.query.id) {
+    // dummy test 용 타계정 페이지
+    var id = parseInt(req.query.id);
+
+    User.dummyShowOther(id, page, count, function(err, user){
+      if (err) {
+        return next(err);
+      }
+      res.send(user);
+    });
+  } else {
+
+    User.dummyShowMe(page, count, function(err, user){
+      if (err) {
+        return next(err);
+      }
+      res.send(user);
+    });
+  }
+});
+
+router.post('/', function(req, res, next){
+
+  if (!req.body.email || !req.body.nickname || !req.body.password) {
+    res.send({
+      error: {
+        message: '회원 가입을 실패했습니'
+      }
+    });
+  } else {
+    var userInfo = {};
+    userInfo.email = req.body.email;
+    userInfo.password = req.body.password;
+    userInfo.nickname = req.body.nickname;
+
+    userInfo.gender = req.body.gender || '';
+    userInfo.position = req.body.position || ''; // position 필수인지 다시 확인
+    userInfo.genre = req.body.genre || '';
+    userInfo.city = req.body.city || '';
+    userInfo.town = req.body.town || '';
+    
+    User.dummyRegisterUser(userInfo, function(err, result){
+      if (err) {
+        return next(err);
+      }
+      else {
+        if (result) {
+          res.send({
+            message: '회원 가입이 정상적으로 처리되었습니다',
+            dummyMessage: 'dummy test를 위한 입력 데이터 출력입니다',
+            dummyData: userInfo
+          });
+        } else {
+          res.send({
+            error: {
+              message: '회원 가입을 실패했습니'
+            }
+          });
         }
-      ]
-    }
-  });
+      }
+    });
+
+  }
+
+
 });
 
 module.exports = router;
