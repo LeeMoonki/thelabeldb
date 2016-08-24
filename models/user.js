cc91d1c71e5fba8f349998b0fad8cd092aa9c417
LEEMOONKI <shinbaek89@gmail.com>
2016-08-24 12:01:18 +0900

    add users dummy test set

--- models/user.js
+++ models/user.js
@@ -1,7 +1,15 @@
-//더미 이메일 패스워드
-var dummy_email = 'abcd@naver.com';
-var dummy_password = '1234';
-var dummy_id = 1;
+//더미 data
+var dummyUser = {};
+
+dummyUser.dummy_email = 'abcd@naver.com';
+dummyUser.dummy_password = '1234';
+dummyUser.dummy_id = 1;
+dummyUser.dummy_nickname = '닉네임';
+dummyUser.dummy_gender = '남자';
+dummyUser.dummy_position = '보컬';
+dummyUser.dummy_genre = '발라드';
+dummyUser.dummy_city = '서울';
+dummyUser.dummy_town = '관악구';
 
 
 function findByEmail(email, callback) {
@@ -13,10 +21,10 @@ function findByEmail(email, callback) {
     }
     else {
         var user = {};
-        user.id = 1;
-        user.name = 'thelabel';
+        user.id = dummyUser.dummy_id;
+        user.nickname = dummyUser.dummy_nickname;
         user.email = email;
-        user.password = dummy_password;
+        user.password = dummyUser.dummy_password;
         callback(null, user);
     }
 }
@@ -25,7 +33,7 @@ function verifyPassword(password, storedpassword, callback) {
     // if (err) {
     //     return callback(err);
     // }
-    storedpassword = dummy_password;
+    storedpassword = dummyUser.dummy_password;
     if (password !== storedpassword) {
         return callback(null, false);
     }
@@ -36,9 +44,14 @@ function verifyPassword(password, storedpassword, callback) {
 
 function  findUser(userId, callback) {
     var user = {};
-    user.id = dummy_id;
-    user.name = 'thelabel';
-    user.email = dummy_email;
+    user.id = dummyUser.dummy_id;
+    user.nickname = dummyUser.dummy_nickname;
+    user.email = dummyUser.dummy_email;
+    user.gender = dummyUser.dummy_gender;
+    user.position = dummyUser.dummy_position;
+    user.genre = dummyUser.dummy_genre;
+    user.city = dummyUser.dummy_city;
+    user.town = dummyUser.dummy_town;
     callback(null, user);
 }
 
@@ -52,16 +65,16 @@ function dummyShowMe(page, count, callback){
     page: page,
     count: count,
     result: {
-      id: 1,
-      nickname: '닉네임',
+      id: dummyUser.dummy_id,
+      nickname: dummyUser.dummy_nickname,
       image_page: '/usr/desktop/didimdol.jpg',
-      genre: '발라드',
+      genre: dummyUser.dummy_genre,
       post_count: 2,
       data: [
         {
           id: 2,
-          user_id: 1,
-          nickname: '닉네임',
+          user_id: dummyUser.dummy_id,
+          nickname: dummyUser.dummy_nickname,
           filetype: 0,
           file_path: '/usr/desktop/didimdol.mp3',
           date: '2016-08-23',
@@ -69,8 +82,8 @@ function dummyShowMe(page, count, callback){
         },
         {
           id: 5,
-          user_id: 1,
-          nickname: '닉네임',
+          user_id: dummyUser.dummy_id,
+          nickname: dummyUser.dummy_nickname,
           filetype: 0,
           file_path: '/usr/desktop/didimdol2.mp3',
           date: '2016-08-23',
@@ -120,10 +133,38 @@ function dummyShowOther(id, page, count, callback){
   callback(null, user);
 }
 
+function dummyShowProfilePage(callback){
+  var user = {};
+  user.id = dummyUser.dummy_id;
+  user.email = dummyUser.dummy_email;
+  user.nickname = dummyUser.dummy_nickname;
+  user.gender = dummyUser.dummy_gender;
+  user.position = dummyUser.dummy_position;
+  user.genre = dummyUser.dummy_genre;
+  user.city = dummyUser.dummy_city;
+  user.town = dummyUser.dummy_town;
+
+  callback(null, user);
+}
+
 function dummyRegisterUser(user, callback){
 
   callback(null, true);
 
+}
+
+function dummyUpdateUser(user, callback){
+  callback(null, true);
+}
+
+function dummyUpdatePassword(userId, password, newPassword, callback){
+  // userId 를 통해 기존 password를 찾아 받은 password와 비교해야 한다
+  if (dummyUser.dummy_password !== password) {
+    callback(new Error('기존 비밀번호를 다르게 입력했습니다'), false);
+  } else {
+    dummyUser.dummy_password = newPassword;
+    callback(null, true);
+  }
 }
 
 module.exports.findByEmail = findByEmail;
@@ -134,3 +175,7 @@ module.exports.findUser = findUser;
 module.exports.dummyShowMe = dummyShowMe;
 module.exports.dummyShowOther = dummyShowOther;
 module.exports.dummyRegisterUser = dummyRegisterUser;
+module.exports.dummyShowProfilePage = dummyShowProfilePage;
+module.exports.dummyUpdateUser = dummyUpdateUser;
+module.exports.dummyUpdatePassword = dummyUpdatePassword;
+module.exports.dummyUser = dummyUser;
--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,11 @@ var User = require('../models/user');
 var isAuthenticate = require('./common').isAuthenticate;
 
 
-
-/* GET users listing. */
 router.get('/', isAuthenticate, function(req, res, next) {
 
   var page = parseInt(req.query.page) || 1;
   var count = parseInt(req.query.count) || 10;
+  var setting = req.query.setting || false; // req.qury 를 통해 Boolean 값을 넘기면 String이 아닌 Boolean으로 넘어온다
 
   if (!req.user) {
     res.send({
@@ -25,14 +24,37 @@ router.get('/', isAuthenticate, function(req, res, next) {
 
     User.dummyShowOther(id, page, count, function(err, user){
       if (err) {
+        res.send({
+          error: {
+            message: '페이지를 불러오지 못했습니다'
+          }
+        });
         return next(err);
       }
       res.send(user);
     });
+  } else if (setting) {
+    // dummy test 용 프로필 설정 페이지
+    User.dummyShowProfilePage(function(err, result){
+      if (err) {
+        res.send({
+          error: {
+            message: '페이지를 불러오지 못했습니다'
+          }
+        });
+        return next(err);
+      }
+      res.send(result);
+    });
   } else {
-
+    // dummy test 용 내계정 페이지
     User.dummyShowMe(page, count, function(err, user){
       if (err) {
+        res.send({
+          error: {
+            message: '페이지를 불러오지 못했습니다'
+          }
+        });
         return next(err);
       }
       res.send(user);
@@ -42,26 +64,32 @@ router.get('/', isAuthenticate, function(req, res, next) {
 
 router.post('/', function(req, res, next){
 
-  if (!req.body.email || !req.body.nickname || !req.body.password) {
+  if (!req.body.email || !req.body.nickname || !req.body.password || req.user) {
     res.send({
       error: {
         message: '회원 가입을 실패했습니'
       }
     });
   } else {
-    var userInfo = {};
-    userInfo.email = req.body.email;
-    userInfo.password = req.body.password;
-    userInfo.nickname = req.body.nickname;
+    User.dummyUser.dummy_email = req.body.email;
+    User.dummyUser.dummy_password = req.body.password;
+    User.dummyUser.dummy_nickname = req.body.nickname;
 
-    userInfo.gender = req.body.gender || '';
-    userInfo.position = req.body.position || ''; // position 필수인지 다시 확인
-    userInfo.genre = req.body.genre || '';
-    userInfo.city = req.body.city || '';
-    userInfo.town = req.body.town || '';
-    
-    User.dummyRegisterUser(userInfo, function(err, result){
+    User.dummyUser.dummy_gender = req.body.gender || '';
+    User.dummyUser.dummy_position = req.body.position || ''; // position 필수인지 다시 확인
+    User.dummyUser.dummy_genre = req.body.genre || '';
+    User.dummyUser.dummy_city = req.body.city || '';
+    User.dummyUser.dummy_town = req.body.town || '';
+
+    console.log(User.dummyUser);
+
+    User.dummyRegisterUser(User.dummyUser, function(err, result){
       if (err) {
+        res.send({
+          error: {
+            message: '회원 가입을 실패했습니'
+          }
+        });
         return next(err);
       }
       else {
@@ -69,7 +97,7 @@ router.post('/', function(req, res, next){
           res.send({
             message: '회원 가입이 정상적으로 처리되었습니다',
             dummyMessage: 'dummy test를 위한 입력 데이터 출력입니다',
-            dummyData: userInfo
+            dummyData: User.dummyUser
           });
         } else {
           res.send({
@@ -84,6 +112,89 @@ router.post('/', function(req, res, next){
   }
 
 
+});
+
+router.put('/', isAuthenticate, function(req, res, next){
+
+  var pass = req.query.pass || false;
+
+  if (pass) {
+
+    if (!req.body.password || !req.body.new_password || !req.body.re_new_password) {
+      res.send({
+        error: {
+          message: '비밀번호 변경실패'
+        }
+      });
+    } else if (req.body.new_password !== req.body.re_new_password) {
+      res.send({
+        error: {
+          message: '비밀번호 변경실패'
+        }
+      });
+    } else {
+      User.dummyUpdatePassword(req.user.id, req.body.password, req.body.new_password, function (err, result) {
+        if (err) {
+          res.send({
+            error: {
+              message: '비밀번호 변경실패'
+            }
+          });
+          return next(err);
+        } else {
+          if (result) {
+            res.send({
+              message: '비밀번호 변경',
+              dummyMessage: 'dummy test를 위한 입력 데이터 출력입니다',
+              dummyData: User.dummyUser.dummy_password
+            });
+          } else {
+            res.send({
+              error: {
+                message: '비밀번호 변경실패'
+              }
+            });
+          }
+        }
+
+      });
+    }
+
+  } else {
+    User.dummyUser.dummy_id = req.body.id || req.user.id;
+    User.dummyUser.dummy_email = req.body.email || req.user.email;
+    User.dummyUser.dummy_nickname = req.body.nickname || req.user.nickname;
+    User.dummyUser.dummy_gender = req.body.gender || req.user.gender;
+    User.dummyUser.dummy_position = req.body.position || req.user.position;
+    User.dummyUser.dummy_genre = req.body.genre || req.user.genre;
+    User.dummyUser.dummy_city = req.body.city || req.user.city;
+    User.dummyUser.dummy_town = req.body.town || req.user.town;
+
+    User.dummyUpdateUser(User.dummyUser, function (err, result) {
+      if (err) {
+        res.send({
+          error: {
+            message: '변경 할 수 없습니다'
+          }
+        });
+        return next(err);
+      } else {
+        if (result) {
+          res.send({
+            message: '수정 완료',
+            dummyMessage: 'dummy test를 위한 입력 데이터 출력입니다',
+            dummyData: User.dummyUser
+          });
+        } else {
+          res.send({
+            error: {
+              message: '변경 할 수 없습니다'
+            }
+          });
+        }
+      }
+    });
+  }
 });
 
 module.exports = router;
