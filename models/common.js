b898a900ebfb0b520414406b9e87778e1a78d0b9
LEEMOONKI <shinbaek89@gmail.com>
2016-09-05 09:12:17 +0900

    user dummy test

--- models/common.js
+++ models/common.js
@@ -3,8 +3,9 @@ var dbPoolConfig = require('../config/dbPoolConfig');
 
 var dbPool = mysql.createPool(dbPoolConfig);
 
-var hostAddress = 'https://127.0.0.1:4433';
+//var hostAddress = 'https://127.0.0.1:4433';
 //var hostAddress = 'https://localhost:4433';
+var hostAddress = 'https://ec2-52-78-137-47.ap-northeast-2.compute.amazonaws.com:4433';
 
 module.exports.dbPool = dbPool;
 module.exports.hostAddress = hostAddress;
